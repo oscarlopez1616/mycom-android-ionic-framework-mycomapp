62a67036229aef6d508e39adef72f887e89eed97
oscarlopez1616 <oscar.lopez.labrador@gmail.com>
2018-07-12 19:07:06 +0200

    playing

---
+++ src/domain/Balance.ts
@@ -0,0 +1,4 @@
+export interface Balance {
+  quantity: number;
+  currency: string;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Balance} from "../../domain/Balance";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private balance: Balance;
 
+  constructor(public navCtrl: NavController) {
+    this.balance = {
+      "quantity": 200,
+      "currency": "EUR"
+    };
+  }
+
+  public refreshBalance(): void {
   }
 
 }
