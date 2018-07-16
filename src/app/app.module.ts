e428f8cb38955af1740da0910d6e2727ba5b4b29
oscarlopez1616 <oscar.lopez.labrador@gmail.com>
2018-07-16 19:13:52 +0200

    playing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,38 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
 
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import {HomePage} from '../pages/home/home';
+import {TabsPage} from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    TabsPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        TabsPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp).providers,
+        HttpClientModule
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        TabsPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,43 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {Balance} from "../../domain/Balance";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    configUrl = 'assets/config.json';
 
-  private balance: Balance;
+    private balance: Balance;
 
-  constructor(public navCtrl: NavController) {
-    this.balance = {
-      "quantity": 200,
-      "currency": "EUR"
-    };
-  }
 
-  public refreshBalance(): void {
-  }
+    constructor(public navCtrl: NavController, private http: HttpClient) {
+        this.balance = {
+            "quantity": 200,
+            "currency": "EUR"
+        };
+    }
+
+    public refreshBalance(): void {
+        this.configService.getConfig()
+            .subscribe((data: Config) => this.config = {
+                heroesUrl: data['heroesUrl'],
+                textfile:  data['textfile']
+            });
+    }
+
+    public getConfig() {
+        return this.http.get(this.configUrl);
+    }
+
+    public showConfig() {
+        this.configService.getConfig()
+            .subscribe((data: Config) => this.config = {
+                heroesUrl: data['heroesUrl'],
+                textfile:  data['textfile']
+            });
+    }
 
 }
